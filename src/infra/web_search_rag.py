8c980f331a17476060b3ea95101acb68bd03f9a9
Lemontea <lemontea.Tom@gmail.com>
2025-11-10 03:35:41 +0800

    Disable duckduckgo-search usage and add security warning
    Disabled the use of the duckduckgo-search library due to privacy concerns and added a security warning regarding its recent changes.

--- src/infra/web_search_rag.py
+++ src/infra/web_search_rag.py
@@ -1,11 +1,28 @@
 # Sample RAG chatbot
 
+security_warning = """
+Update (2025 Nov):
+Due to what seem like to be increased enforcement from duckduckgo, the `duckduckgo-search` library, which is pretty
+much the only python lib providing api-like access, doesn't really work in more recent time. By itself, this would be
+sad but understandable. However, the library was updated with a drastic change in what it actually does:
+- it is now a metasearch engine - **and it includes backend that privacy sensitive user may find completely unacceptable**
+- this change is **implicit and automatic with no warning given to user** - unsuspecting user may end up accidentally sabotaging their privacy
+- And the name is reinterpreted - **DDGS now means Dux Distributed Global Search**, which has the same initial as duck duck go search,
+  so that user who didn't dig deep may not realize the full ramification of continuing to use the library (by contrast, if the library is
+  renamed to say "metasearch aggregator", then at least privacy sensitive user may more easily notice what's going on and make an informed
+  choice themselves.)
+
+Now, I have nothing against that library itself - they have full right to decides what to do with the library. However,
+given the situation listed above, in an abundance of caution, I have disabled the relevant code below. As an end user,
+it is up to you to decide whether you want to proceed or not.
+"""
+
 from transformers import AutoTokenizer
 
 import json
 import re
 
-from duckduckgo_search import DDGS
+#from duckduckgo_search import DDGS
 
 from llama_index.readers.web import SimpleWebPageReader
 
@@ -21,9 +38,12 @@ def extract_action(text):
     cap_grp = re.search("```(json|js|javascript)?([\s\S]*)```", text).group(2)
     return json.loads(cap_grp)
 
+# Uncomment only if you really know what you are doing
+#def web_search(q):
+#    results = DDGS().text(q + " filetype:html", max_results=10)
+#    return results
 def web_search(q):
-    results = DDGS().text(q + " filetype:html", max_results=10)
-    return results
+    raise RuntimeError(security_warning)
 
 
 #def gen_docs(search_results):
